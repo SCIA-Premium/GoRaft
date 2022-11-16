e24cbe6a4ee963b4f471b4b7db6fd00b2f1e68fd
Bernet <philippe.bernet@epita.fr>
2022-11-16 19:35:25 +0100

    refa(src_repl): move repl function in other file

--- src/raft.go
+++ src/raft.go
@@ -201,34 +201,6 @@ func (n *Node) Start() {
 	go n.Step()
 }
 
-func (n *Node) Stop(false_arg string, false_res *string) error {
-	n.State = Dead
-	log.Printf("Node %d [%s]\n", n.Peer_ID, n.State)
-	return nil
-}
-
-func (n *Node) ChangeSpeed(new_speed_string string, false_res *string) error {
-
-	var new_speed NodeSpeed
-	switch new_speed_string {
-	case "high":
-		new_speed = NodeSpeed{"high", 300}
-	case "medium":
-		new_speed = NodeSpeed{"medium", 600}
-	case "low":
-		new_speed = NodeSpeed{"low", 1000}
-	default:
-		log.Printf("ChangeSpeed : unknow speed specification : %s", new_speed_string)
-		return nil
-	}
-	
-	log.Printf("Node %d [%s]: old speed state %s -> new speed state %s \n", n.Peer_ID, n.State, n.Speed.key, new_speed.key)
-	
-	n.Speed = new_speed
-
-	return nil
-}
-
 func (n *Node) startRpc(port string) {
 	rpc.Register(n)
 	rpc.HandleHTTP()
---
+++ src/repl.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+)
+
+func (n *Node) Stop(false_arg string, false_res *string) error {
+	n.State = Dead
+	log.Printf("Node %d [%s]\n", n.Peer_ID, n.State)
+	return nil
+}
+
+func (n *Node) ChangeSpeed(new_speed_string string, false_res *string) error {
+
+	var new_speed NodeSpeed
+	switch new_speed_string {
+	case "high":
+		new_speed = NodeSpeed{"high", 300}
+	case "medium":
+		new_speed = NodeSpeed{"medium", 600}
+	case "low":
+		new_speed = NodeSpeed{"low", 1000}
+	default:
+		log.Printf("ChangeSpeed : unknow speed specification : %s", new_speed_string)
+		return nil
+	}
+	
+	log.Printf("Node %d [%s]: old speed state %s -> new speed state %s \n", n.Peer_ID, n.State, n.Speed.key, new_speed.key)
+	
+	n.Speed = new_speed
+
+	return nil
+}
